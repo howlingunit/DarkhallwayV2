a402f5ce53dc1439ccf786fdd3852595b2b947a0
howlingunit <tyler_pitt@outlook.com>
2022-02-03 15:01:10 +0000

    the first basic working version

--- srv.js
+++ srv.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.static('static'));
 
-app.post("/initUser", express.json(), DH.initUser)
+app.post("/initUser", express.json(), DH.initUser);
+
+app.post("/sendMsg", express.json(), DH.sendMsg);
+
+app.post("/check", express.json(), DH.check);
 
 app.listen(8080);
--- static/script.js
+++ static/script.js
@@ -4,7 +4,13 @@ function init(){
 
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function msgInit(){
+    const sendBtn = document.querySelector("#sendMsg");
+    sendBtn.addEventListener("click", sendMsg);
     const userName = document.querySelector("#username").value;
     const nameBox = document.querySelector("#nameBox");
     const msgBox = document.querySelector("#msgBox");
@@ -20,8 +26,7 @@ async function msgInit(){
     })
 
     if(response.ok){
-        const msgs = await response.json();
-        running(msgs);
+        running();
 
     } else{
         console.log("svr error");
@@ -29,16 +34,46 @@ async function msgInit(){
 }
 
 function writeMsgs(msgs){
+    const oldMsgList = document.querySelectorAll(".msg");
+    for(i of oldMsgList){
+        i.remove();
+    }
     const msgList = document.querySelector("#msgList");
     for(item of msgs){
         const msg = document.createElement("li");
+        msg.classList.add("msg")
         msg.textContent = `${item.user}:${item.msg}`;
         msgList.appendChild(msg);
     }
 }
 
-function running(msgs){
-    writeMsgs(msgs);
+async function sendMsg(){
+    const msg = document.querySelector("#msgInput").value;
+    const user = document.querySelector("#username").value;
+    const payload = {"user":user, "msg":msg};
+    console.log(payload);
+    const response = await fetch("sendMsg", {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(payload)
+    });
+    running()
+
+}
+async function running(){
+    const username = document.querySelector("#username").value;
+    while(true){
+        const response = await fetch("check", {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify([username])
+        });
+        const msgs = await response.json();
+        console.log(msgs);
+        writeMsgs(msgs);
+        sleep(1000);
+
+    }
 }
 
 window.addEventListener("load", init)
